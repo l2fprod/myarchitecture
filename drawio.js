ea905d5e21ac5e781b92906bdce13edcbc3b778b
Frederic Lavigne <frederic.lavigne@ibm.com>
2022-11-17 14:58:09 -0600

    💄 new icon style based on design guidelines

--- drawio.js
+++ drawio.js
@@ -93,48 +93,28 @@ function DrawIOLibrary() {
       `data:image/svg+xml,${imageContentBase64}` :
       `data:image/png,${imageContentBase64}`;
 
-    // icon only
-    const styleObject = {
-      aspect: 'fixed',
-      perimeter: 'ellipsePerimeter',
-      html: 1,
-      align: 'center',
-      shadow: 0,
-      dashed: 0,
-      fontColor: '#4277BB',
-      fontSize: 12,
-      spacingTop: 3,
-      'image;image': imageData,
-      strokeColor: '#4277BB',
-      strokeWidth: 2,
-      labelBackgroundColor: 'none',
-      rounded: 0,
-    }
-
-    let style = '';
-    Object.keys(styleObject).forEach((key, index) => {
-      style = style + ((index > 0) ? ';' : '') + `${key}=${styleObject[key]}`
-    });
-
-    const linkElement = link ? `link="${link}" ` : '';
     const graphModel = `
 <mxGraphModel>
   <root>
-    <mxCell id="0"/>
-    <mxCell id="1" parent="0"/>
-    <UserObject ${linkElement}id="2">
-      <mxCell style="${style}" vertex="1" parent="1">
-        <mxGeometry width="60" height="60" as="geometry"/>
-      </mxCell>
-    </UserObject>
+    <mxCell id="0" />
+    <mxCell id="1" parent="0" />
+    <mxCell id="2" value="" style="group;fontFamily=IBM Plex Sans;verticalAlign=top;labelPosition=center;verticalLabelPosition=bottom;align=center;spacingLeft=0;spacingRight=0;spacingBottom=4;whiteSpace=wrap;html=1;fontStyle=0;fontSize=14;strokeOpacity=0;strokeColor=none;strokeWidth=0;spacing=0;connectable=1;" vertex="1" connectable="0" parent="1">
+      <mxGeometry width="48" height="48" as="geometry" />
+    </mxCell>
+    <mxCell id="3" value="" style="editable=0;moveable=0;rounded=0;whiteSpace=wrap;html=1;spacingLeft=0;verticalAlign=top;fontFamily=IBM Plex Sans;fontSize=14;align=center;spacingBottom=0;spacingRight=0;labelPosition=center;verticalLabelPosition=bottom;spacing=0;strokeColor=#999999;connectable=0;allowArrows=0;" vertex="1" parent="2">
+      <mxGeometry width="48" height="48" as="geometry" />
+    </mxCell>
+    <mxCell id="4" value="" style="editable=0;moveable=0;rounded=0;shape=image;verticalLabelPosition=bottom;labelBackgroundColor=default;verticalAlign=top;aspect=fixed;imageAspect=0;image=${imageData};imageBorder=none;perimeterSpacing=0;imageBackground=none;sketch=0;connectable=0;allowArrows=0;" vertex="1" parent="2">
+      <mxGeometry x="14" y="14" width="20" height="20" as="geometry" />
+    </mxCell>
   </root>
 </mxGraphModel>`.replace(/  /g, '').replace(/\n/g, '');
 
     const xmlContent = self.compress(graphModel, false);
     return {
       xml: xmlContent,
-      w: 60,
-      h: 60,
+      w: 48,
+      h: 48,
       aspect: 'fixed',
       title: entities.encode(iconTitle),
     };
